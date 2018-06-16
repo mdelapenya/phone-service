d045dd786c3a4c3e082856d1646044505a0ca233
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-06-16 12:28:54 +0200

    Add Get an Set methods

--- redis.go
+++ redis.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/go-redis/redis"
@@ -10,14 +9,32 @@ import (
 var serverURL = os.Getenv("SERVER_URL")
 
 // redisClient Creates a new RedisClient
-func redisClient() {
-	client := redis.NewClient(&redis.Options{
+func redisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
 		Addr:     serverURL + ":6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+}
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	// Output: PONG <nil>
+// Get gets the value of a key from the datastore
+func Get(key string) string {
+	client := redisClient()
+
+	val, err := client.Get(key).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return val
+}
+
+// Set sets a key and its value to the datastore
+func Set(key string, value string) {
+	client := redisClient()
+
+	err := client.Set(key, value, 0).Err()
+	if err != nil {
+		panic(err)
+	}
 }
